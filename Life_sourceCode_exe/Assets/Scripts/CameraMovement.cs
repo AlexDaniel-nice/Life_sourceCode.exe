11cad8d226a7e7d44dc9ca62b727e2ef5b7a7a55
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2021-11-02 12:50:40 +0200

    control al camerei rudimentar

---
+++ Life_sourceCode_exe/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform TargetPoz1;
+    public Transform TargetPoz2;
+    public GameObject NextPhazeBTN;
+
+    private bool isUpView;
+    private bool isCodeView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isCodeView = NextPhazeBTN.GetComponent<nextPhaze>().CodePhaze;
+
+        if (isCodeView==true)
+        {
+            this.transform.position = TargetPoz2.position;
+            this.transform.rotation = TargetPoz2.rotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.position = TargetPoz1.position;
+            this.transform.rotation = TargetPoz1.rotation;
+        }
+    }
+}
--- Life_sourceCode_exe/Assets/Scripts/nextPhaze.cs
+++ Life_sourceCode_exe/Assets/Scripts/nextPhaze.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class nextPhaze : MonoBehaviour
 {
+    public bool CodePhaze;
+
     public void NextPhaze()
     {
         Debug.Log("Next part");
+        CodePhaze = true;
     }
 }

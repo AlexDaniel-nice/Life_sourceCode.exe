f1232387eda779d918a66c9b17423c789e5a83f4
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2021-12-25 14:56:15 +0200

    code part
    am adaugat o noua scena pt a testa partea de cod
    cred  ca am gasit o solutie pt a trece la urmatoarele linii.

---
+++ Life_sourceCode_exe/Assets/Scripts/codeInterpretor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.TextCore;
+
+
+public class codeInterpretor : MonoBehaviour
+{
+    public InputField CodeWrite;
+    public string defaultBeginMess;
+
+
+    public void onEndEdit(string input)
+    {
+        if (input == "enable")
+        {
+            CodeWrite.text = '\n' + "entering advamced config";
+            Debug.Log("entering advamced config");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CodeWrite.text = '\n'+ defaultBeginMess + '\n';
+        
+    }
+
+
+    public void onClik()
+    {
+        CodeWrite.text = "enable" + '\n' + "entering advanced conf";
+    }
+}

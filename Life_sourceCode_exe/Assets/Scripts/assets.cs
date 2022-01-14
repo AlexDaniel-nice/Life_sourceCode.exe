58875d416dca8be1b545827c6c4704e6960735ed
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-01-14 23:12:57 +0200

    parte 3
    am incercat sa fac o functie care sa imi spauneze ARNm

---
+++ Life_sourceCode_exe/Assets/Scripts/assets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assets : MonoBehaviour
+{
+    private static assets _instance;
+
+    public static assets instance()
+    {
+            return _instance;
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public Transform Friend;
+
+}

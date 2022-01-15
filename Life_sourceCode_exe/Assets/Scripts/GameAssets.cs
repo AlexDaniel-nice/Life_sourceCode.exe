650c073e20a56d43736bc36b9efaa082da69ea9b
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-01-15 19:05:23 +0200

    advances to part 3

---
+++ Life_sourceCode_exe/Assets/Scripts/GameAssets.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    //Referinta la aset-uri:
+    public GameObject Friend;
+
+    private static GameAssets instance;
+
+    public static GameAssets GetInstance
+    {
+        get
+        {
+            if (instance == null)
+                instance = (Instantiate(Resources.Load("GameAssets")) as GameObject).GetComponent<GameAssets>();
+            return instance;
+        }
+        
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
---
+++ Life_sourceCode_exe/Assets/Scripts/levelGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private float TimeToSpawnFriend;
+
+    void Start()
+    {
+        SpawnFriend(10, 10, 10);
+        SpawnFriend(10, 5, 5);
+        SpawnFriend(-10, 10, 10);
+    }
+
+    private void SpawnFriend(float xPoz, float yPoz, float zPoz)
+    {
+        GameObject FriendEntity = Instantiate(GameAssets.GetInstance.Friend);
+        FriendEntity.transform.position = new Vector3(xPoz, yPoz, zPoz);
+    }
+
+    //de gasit o metoda mai optima pt codul din update
+    //de reparat functia timer
+    private void Update()
+    {
+        float Timer = TimeToSpawnFriend;
+
+        Timer -= Time.deltaTime;
+        if (Timer<=0)
+        {
+            Timer = TimeToSpawnFriend;
+
+            //In loc de 0 pun o functie care calculeaza o valoare random pt pozitia obiectului
+           // SpawnFriend(0, 10, 0);
+        }
+
+    }
+    private void TimeForNewFriend(float nextFriendInNsec)
+    {
+        
+    }
+
+}

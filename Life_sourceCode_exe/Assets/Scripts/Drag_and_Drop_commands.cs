68b6d76875eaf8e2e408e210348e52e4ef1ab49b
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-04-13 13:32:04 +0300

    Code Part
    1. Pune componenta dra_and_drop pe gameobject Comenzi
    
    2. Gaseste o metoda sa spaunei blocurile cu comenzi

---
+++ Life_sourceCode_exe/Assets/Scripts/Drag_and_Drop_commands.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag_and_Drop_commands : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    [SerializeField] private Canvas Canvas;
+
+    private RectTransform butonComenzi;
+    private CanvasGroup InteractableObjects;
+
+    private void Awake()
+    {
+        butonComenzi = this.GetComponent<RectTransform>();
+        InteractableObjects = this.GetComponent<CanvasGroup>();
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OneginDrag");
+        InteractableObjects.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("onDrag");
+        butonComenzi.anchoredPosition += eventData.delta / Canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        InteractableObjects.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pressed");
+        // Spawn copy of button
+        //butonComenzi = instance.getComponent<RectTransform>();
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Life_sourceCode_exe/Assets/Scripts/comenzi_slot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class comenzi_slot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        // cod efectiv care pune ce am carat cu mousul unde vreau;
+    }
+
+}

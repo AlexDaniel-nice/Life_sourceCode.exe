a27c347e0885604da2899be5e76c646104ca24c5
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-04-28 17:02:53 +0300

    Added loading feature

---
+++ Life_sourceCode_exe/Assets/Scripts/Loader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        GameScene,
+        loadingScene,
+        CodeWriting,
+    }
+
+    private static Action onLoaderCallback;
+    public static void Load (Scene scene)
+    {
+        onLoaderCallback = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+
+        SceneManager.LoadScene(Scene.loadingScene.ToString());
+
+    }
+
+    public static void LoaderCallback()
+    {
+        if (onLoaderCallback != null)
+        {
+            onLoaderCallback();
+            onLoaderCallback = null;
+        }
+    }
+}
---
+++ Life_sourceCode_exe/Assets/Scripts/loaderCallbackScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loaderCallbackScript : MonoBehaviour
+{
+    bool isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (isFirstUpdate)
+        {
+            isFirstUpdate = false;
+            Loader.LoaderCallback();
+        }
+    }
+}

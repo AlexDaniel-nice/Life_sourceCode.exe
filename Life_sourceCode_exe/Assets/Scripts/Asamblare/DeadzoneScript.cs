6cf71b10e160392577cad0b4a2329ee88ac25301
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-04-29 20:55:07 +0300

    respaunarea obiectelor incercarea 1

---
+++ Life_sourceCode_exe/Assets/Scripts/Asamblare/DeadzoneScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeadzoneScript : MonoBehaviour
+{
+    public static EventHandler onDeadzoneEnter;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+            onDeadzoneEnter?.Invoke(this, EventArgs.Empty);
+            Debug.Log("We are here respawning");
+        
+    }
+}

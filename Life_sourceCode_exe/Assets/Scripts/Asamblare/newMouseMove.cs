e9f2d2e9510e5dd08158db8712e255930e1a7486
AlexDaniel-nice <78504371+AlexDaniel-nice@users.noreply.github.com>
2022-04-29 20:07:12 +0300

    Objects can now be moved

---
+++ Life_sourceCode_exe/Assets/Scripts/Asamblare/newMouseMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newMouseMove : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Transform fixedPozition;
+
+    private Rigidbody _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixObjInPlace()
+    {
+            _rb.transform.position = fixedPozition.position;
+    }
+    private void HandleCursorMovement()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit rayCastHit, float.MaxValue, layerMask))
+        {
+            transform.position = rayCastHit.point;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        HandleCursorMovement();
+        _rb.isKinematic = true;
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+        {
+            FixObjInPlace();
+            return;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        _rb.isKinematic = false;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Life_sourceCode.exe
 Un joc de programare creat pentru Infoeducatie editia 2022
 
+Componenta echipei:
+	- Vlad Ruben
+	- Barbu Alexandru Daniel
+
+Impartirea task-urilor:
+	- cod: Barbu Alexandru Daniel
+	- sunet: Vlad Ruben
+	- viziune artistica si directia viitoare a proiectului: Vlad Ruben si Barbu Alexandru Daniel
+
 Pentru crearea mai rapida a scenelor am folosit parti din urmatoarele pachete:
   - https://assetstore.unity.com/packages/3d/vegetation/trees/low-poly-tree-pack-57866
   - https://assetstore.unity.com/packages/3d/props/tools/free-laboratory-pack-123782
